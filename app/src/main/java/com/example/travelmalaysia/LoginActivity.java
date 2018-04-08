86b91c49d77438b12ffbb0ab0324fa7e673753db
Kevin Lu <kevin999990@hotmail.com>
2018-04-08 23:07:41 +0800

    Do not show keyboard while Login and Register start

--- app/src/main/java/com/example/travelmalaysia/LoginActivity.java
+++ app/src/main/java/com/example/travelmalaysia/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.travelmalaysia;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,7 +42,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
-
-
     }
 }
